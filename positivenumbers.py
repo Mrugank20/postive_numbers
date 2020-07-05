7f91778ea73613c0a52d11591242c502bc406c18
Mrugank20 <67855792+Mrugank20@users.noreply.github.com>
2020-07-05 14:44:46 +0530

    created by mrugank
    this program runs the output so as to print the positive numbers from the list

---
+++ positivenumbers.py
@@ -0,0 +1,21 @@
+list1=[12,-7,5,64,-14]
+for numbers in list1:
+  if numbers > 0:
+    print(numbers)
+
+
+
+
+
+
+list2=[12,14,-95,3]
+new_list=[]
+for items in list2:
+  if items > 0:
+    new_list.append(items)
+print(new_list)
+    
+   
+
+    
+  
